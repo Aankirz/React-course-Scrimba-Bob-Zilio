343e1de2bd34c4c99cf3e488a5d81702e2334eb1
Aankirz <sahuankit453@gmail.com>
2022-08-18 23:32:03 +0530

    ok

---
+++ React-Project-1/components/Navbar.js
@@ -0,0 +1,19 @@
+/**
+Challenge: Complete the Navbar to match the design
+
+Hint: use the Figma file for the most accurate peek at the design
+(colors, sizes, fonts, etc.)
+*/
+
+import React from "react"
+
+export default function Navbar() {
+    return (
+   <div className="nav">
+       <img src="../images/react-icon-small.png" alt="React-Logo" />
+        <h3>ReactFacts</h3>
+        <h4>React Course - Project 1</h4>       
+   </div>
+
+    )
+}
